d12925c3b0dc3a4a0beb76ae4724bf9a88c0ca9b
Ivlgames <ian.van.lier@gmail.com>
2017-12-07 16:35:12 +0100

    Enable RDP on remote pc's script
    Powershell script to enable RDP on remote computers, you can pass in a .csv/.txt or just a single pc dns name.

---
+++ EnableRemotePCRDP.ps1
@@ -0,0 +1,69 @@
+﻿#Function to enable RDP on another pc, pass in a .csv .txt or a single pc name and wether to enable or disable them for rdp
+function SetRemotePcRDP([parameter(Mandatory=$true)][string]$RemoteComputer,[parameter(Mandatory=$true)][int]$EnableOrDisable)
+{
+    #Tests if we passed in a path (txt or csv) or a single pc name
+    if((Test-Path $RemoteComputer)) {
+         Write-Host "passed in a valid path"
+
+          Get-Content $RemoteComputer |  Foreach-Object {          
+          RemoteRDPHandler -RemoteComputer $_ -EnableOrDisable $EnableOrDisable
+          }
+
+    }
+    else{
+        RemoteRDPHandler -RemoteComputer $RemoteComputer -EnableOrDisable $EnableOrDisable
+    }
+} 
+
+function RemoteRDPHandler($RemoteComputer, $EnableOrDisable){
+     write-host "Changing RDP settings on $RemoteComputer" -ForegroundColor Yellow
+     
+     if ($EnableOrDisable -eq 0)
+        {
+
+         Invoke-Command –Computername $RemoteComputer {
+    
+            # Enables RDP on remote ps (value 1 to disable, 0 to enable)	
+            Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
+            # Enables RDP traffic firewall rule	
+            Enable-NetFirewallRule -DisplayGroup "Remote Desktop"                  
+
+            } 
+              Write-Host "Enabled RDP $RemoteComputer `n" -ForegroundColor Green
+    
+         }
+        else{
+        Invoke-Command –Computername $RemoteComputer {
+            # Enables RDP on remote ps (value 1 to disable, 0 to enable)	
+            Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 1
+            # Disable RDP traffic firewall rule	
+            Disable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+            }
+
+            
+            Write-Host "Disabled RDP $RemoteComputer `n" -ForegroundColor Red
+        }
+
+}
+
+function PrintPcNames($InTextFile){
+
+if(!(Test-Path $InTextFile)) {
+    Write-Host "passed in a single pc name"}
+
+
+else {
+    Write-Host "Passed in text file"
+
+    
+    Get-Content $InTextFile |  Foreach-Object {
+    Write-Host "$_"
+    }
+}
+
+}
+
+
+
+
