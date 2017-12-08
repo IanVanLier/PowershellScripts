1071fa95a3ddb049a89fa44edb39c1cfd02a567f
Ian Van Lier <ivanlier@vanhessen.be>
2017-12-08 14:05:54 +0100

    Generate excel file with all VM's on ESX hosts
    added export-excel from https://github.com/dfinke/ImportExcel

--- GenerateVMList.ps1
+++ GenerateVMList.ps1
@@ -8,7 +8,7 @@ $VMHostCredentials = (get-credential)
         Connect-VIServer -Server $_ -Credential $VMHostCredentials  
         }       
          
-    $ExportVmList =  Get-VM | Select -Property VMHost, name ,@{N='IP';E={[string]::Join(',',$_.Guest.IPAddress)}},@{N="DNS";E={[string]::Join(',',($_.ExtensionData.Guest.IpStack.DnsConfig.IpAddress))}} 
-    $ExportVmList |Export-Excel -NoNumberConversion IP, DNS $ExportPath
+    $ExportVmList =  Get-VM | Select -Property VMHost, name ,@{N='IP';E={[string]::Join(',',$_.Guest.IPAddress)}},@{N="DNS";E={[string]::Join(',',($_.ExtensionData.Guest.IpStack.DnsConfig.IpAddress))}}
+    $ExportVmList |Export-Excel -TitleBold -TitleFillPattern Solid -TitleBackgroundColor AliceBlue -BoldTopRow -NoNumberConversion IP, DNS $ExportPath
     }
 }
