2787148a27cf05f536351d293fc5ae191bcdff15
Ian Van Lier <ivanlier@vanhessen.be>
2017-12-08 11:53:51 +0100

    Generate excel file with all VM's on ESX host
    Function to generate an excel file with IP address and hostname for VMware ESXi hosts.

--- GenerateVMList.ps1
+++ GenerateVMList.ps1
@@ -158,13 +158,13 @@ function GenerateVMList([parameter(Mandatory=$true)][string]$VMHostList ,[parame
 
 $VMHostCredentials = (get-credential)
 
-if(Test-Path $VMHostList){
+    if(Test-Path $VMHostList){
     
-    Get-Content $VMHostList |  Foreach-Object {
-    Connect-VIServer -Server $_ -Credential $VMHostCredentials  
-    }       
+        Get-Content $VMHostList |  Foreach-Object {
+        Connect-VIServer -Server $_ -Credential $VMHostCredentials  
+        }       
          
-     $ExportVmList =  Get-VM | Select -Property VMHost, name ,@{N='IP';E={[string]::Join(',',$_.Guest.IPAddress)}},@{N="DNS";E={[string]::Join(',',($_.ExtensionData.Guest.IpStack.DnsConfig.IpAddress))}} 
+    $ExportVmList =  Get-VM | Select -Property VMHost, name ,@{N='IP';E={[string]::Join(',',$_.Guest.IPAddress)}},@{N="DNS";E={[string]::Join(',',($_.ExtensionData.Guest.IpStack.DnsConfig.IpAddress))}} 
     Export-Xls $ExportVmList $ExportPath
-}
+    }
 }
